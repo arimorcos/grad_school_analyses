7244a8cb6dabbf33ffeec1376066177059412b72
Ari Morcos <arimorcos@gmail.com>
2015-02-04 12:45:41 -0500

    Added statistics to plotVecMag and choose factors
    Added statistics to plotVectorMagnitude and the ability to choose which factors are used

--- State Space Analyses/Trajectories/Vectors/plotVectorMagnitude.m
+++ State Space Analyses/Trajectories/Vectors/plotVectorMagnitude.m
@@ -1,10 +1,11 @@
-function plotVectorMagnitude(segVectors,groupBy)
+function plotVectorMagnitude(segVectors,groupBy,whichFactors)
 %plotMeanSegmentVectors.m Plots mean segment vectors for each unique
 %condition
 %
 %INPUTS
 %segVectors - table output by getSegVectors
 %groupBy - names of variables to group according to 
+%whichFactors - which factors to calculate angle on. Default is all 
 %
 %ASM 2/15
 
@@ -28,6 +29,14 @@ vectors = segVectors.vector;
 %remove unnecessary variables
 segVectors = segVectors(:,groupBy);
 
+%whichFactors
+if nargin < 3 || isempty(whichFactors)
+    whichFactors = 1:length(vectors{1});
+end
+
+%crop factors 
+vectors = cellfun(@(x) x(whichFactors),vectors,'UniformOutput',false);
+
 %get nTrials
 nTrials = size(segVectors,1);
 
@@ -41,6 +50,7 @@ varNames = segVectors.Properties.VariableNames;
 %loop through each condition and generate mean vectors
 meanMag = nan(1,nConds);
 semMag = nan(1,nConds);
+vectorMags = cell(1,nConds);
 for condInd = 1:nConds
     
     %find matching indices
@@ -51,11 +61,29 @@ for condInd = 1:nConds
     
     %get magnitude
     getMagnitude = @(x) sqrt(sum(x.^2));
-    vectorMags = cell2mat(cellfun(getMagnitude, vectors(indMatch), 'UniformOutput', false));
+    vectorMags{condInd} = cell2mat(cellfun(getMagnitude, vectors(indMatch), 'UniformOutput', false));
 
     %compute mean and sem of each
-    meanMag(condInd) = nanmean(vectorMags);
-    semMag(condInd) = calcSEM(vectorMags);
+    meanMag(condInd) = nanmean(vectorMags{condInd});
+    semMag(condInd) = calcSEM(vectorMags{condInd});
+end
+
+%calculate statistics
+statsPairs = nan(nchoosek(nConds,2),2);
+pairPVal = nan(nchoosek(nConds,2),1);
+pairInd = 1;
+for firstCond = 1:nConds
+    for secondCond = firstCond+1:nConds
+        %store pair 
+        statsPairs(pairInd,1) = firstCond;
+        statsPairs(pairInd,2) = secondCond;
+        
+        %calc pVal 
+        [~,pairPVal(pairInd)] = ttest2(vectorMags{firstCond},vectorMags{secondCond});
+        
+        %increment pairInd
+        pairInd = pairInd + 1;
+    end
 end
 
 %create figure 
@@ -66,6 +94,11 @@ hold(axH,'on');
 %create bar chart 
 barH = barwitherr(semMag, meanMag);
 barH.error.LineWidth = 2;
+
+%add statistics 
+criteria = [0.05 1e-2 1e-3];
+% criteria = criteria/sqrt(length(pairPVal)); %bonferonni correct
+statsH = sigstar(num2cell(statsPairs,2),pairPVal,[],[],criteria);
 
 %set prober tick labels 
 strLabels = convertTableToLegendString(uniqueConds);
